a010390941c332217c73f9c76adc9ad89c573601
Omar Khaled <omar.khaled@egabi.com>
2024-06-01 23:15:41 +0300

    updated dist folder

---
+++ src/components/SecPage.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST</h1>
+</template>
+
+<style scoped>
+    h1 {
+        font-size: 64px;
+        text-align: center;
+        color: #fff;
+    }
+</style>
